f16d57fd7711095b3ab305f9527ddc2747e66b93
SerthK <khamsoda@yahoo.com>
2024-10-29 06:19:46 +0000

    Add player model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -11,14 +12,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    player_model = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0,0,0))
+        player_model.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
